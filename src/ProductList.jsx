e2b88480fb1fb622912330ebc92728ed37f96188
to774 <tom323hds@protonmail.com>
2024-12-04 13:24:37 -0500

    product-list component layout

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,18 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatchEvent(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true, //Set the product name as key and value as true to indicate it's added to cart.
+        }))
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -291,5 +299,6 @@ const handlePlantsClick = (e) => {
     </div>
     );
 }
+
 
 export default ProductList;
